3e0418b8b51e20255e59acd04d60c75c3ec4e951
nicok <khamsophan@yahoo.fr>
2024-03-05 11:31:44 +0100

    Fix css collapse

--- kasa/src/components/Rating.jsx
+++ kasa/src/components/Rating.jsx
@@ -2,13 +2,18 @@ import '../styles/Rating.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
-function Rating({rating}) {
+function Rating({ rating }) {
     const range = [1, 2, 3, 4, 5]
     return (
 
         <div className='rating'>
             {range.map((rangeElem) => rating >= rangeElem ?
-             <span key={rangeElem.toString()} className='rated'><FontAwesomeIcon icon={faStar} /></span> : <span className='unrated'><FontAwesomeIcon icon={faStar} /></span>
+                <span key={rangeElem.toString()} className='rated'>
+                    <FontAwesomeIcon icon={faStar} />
+                </span>
+                : <span className='unrated'>
+                    <FontAwesomeIcon icon={faStar} />
+                </span>
             )}
         </div>
 
