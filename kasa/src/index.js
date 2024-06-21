73f02a215e89c3e7a26c7350114f7e27f3720388
nicok <khamsophan@yahoo.fr>
2024-06-21 10:45:11 +0200

    config gh page

--- kasa/src/index.js
+++ kasa/src/index.js
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <React.StrictMode>
-      <Router>
+      <Router basename='/Kasa'>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
